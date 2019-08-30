992d4c80c0931042a31278fec525b4828fd9cc6e
Becky Zorn <bczorn@gmail.com>
2019-08-30 18:07:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,12 @@ def summon_captain_planet(array)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array) # code an argument here
+  if array.length > 4
+    true
+  else
+    false
+  end
 end
 
 def find_the_cheese# code an argument here
