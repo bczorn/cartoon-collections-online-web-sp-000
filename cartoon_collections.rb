440274e140bcd35fb2e25aac95d7dcbc6b265701
Becky Zorn <bczorn@gmail.com>
2019-08-30 17:53:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves) # code an argument here
+  dwarves.each.with_index(1) do |x, i|
+   puts "#{i}. #{x}"
+  end
 end
 
 def summon_captain_planet# code an argument here
